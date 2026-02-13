22021e657b9b633182351f5ab246d7f7588b7631
aryanpaul2107-collab <aryan.paul2107@gmail.com>
2026-02-13 20:58:55 +0530

    Update road.py

--- submissions/game/road.py
+++ submissions/game/road.py
@@ -1,1 +1,16 @@
+"""
+Road Class
+Handles the racing track rendering and animation
+"""
 
+import pygame
+from config import *
+
+
+class Road:
+    """Animated racing road/track"""
+    
+    def __init__(self):
+        self.width = ROAD_WIDTH
+        self.speed = ROAD_SPEED
+        self.line_offset = 0
